8ad0f22bed85bb41d4384689215bd607b2dce421
eybbus <eythorfreyr94@gmail.com>
2020-01-16 21:07:47 +0000

    greifinn refactored as a function and inserts into database

--- Scrapes/webscrapingGreifinn.py
+++ Scrapes/webscrapingGreifinn.py
@@ -1,38 +1,39 @@
-import bs4
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
-from string import digits
+import requests
+from bs4 import BeautifulSoup
+import Scrapes.scrapeMananger as ScrapeMananger
+import re
 
-myUrl = 'https://www.greifinn.is/pizza/index/pizza#center'
-
-
-uClient = uReq(myUrl)
-
-page_html = uClient.read()
-uClient.close()
+URL = 'https://www.greifinn.is/pizza/index/pizza#center'
 
 
-page_soup = soup(page_html, "html.parser")
+def scrape_greifinn():
+    page = requests.get(URL)
+    page_soup = BeautifulSoup(page.content, "html.parser")
 
-pizzas = page_soup.find(id="pizzaMenu").findAll("li")
+    pizzas = page_soup.find(id="pizzaMenu").findAll("li")
+    company_id = ScrapeMananger.insert_or_get_company(name='greifinn', region='norðuland').id
 
-iterPizzas = iter(pizzas)
-next(iterPizzas)
+    iterPizzas = iter(pizzas)
+    next(iterPizzas)
 
-for pizza in iterPizzas:
-	pizzaName = pizza.h4.text
-	pizzaTopping =  pizza.div.text
-	listPizzaTopping = pizzaTopping.split(", ")
-	temp = pizza.find("div", {"class" : "price"}).findAll("div")[0].text
-	pizzaSmallPrice = ''.join(i for i in temp if i.isdigit())
-	temp = pizza.find("div", {"class" : "price"}).findAll("div")[1].text
-	pizzaMidPrice = ''.join(i for i in temp if i.isdigit())
-	temp = pizza.find("div", {"class" : "price"}).findAll("div")[2].text
-	pizzaBigPrice = ''.join(i for i in temp if i.isdigit())
+    for pizza in iterPizzas:
+        pizzaName = pizza.h4.text.lower()
+        pizzaTopping = pizza.div.text
+        listPizzaTopping = pizzaTopping.split(", ")
+        temp = pizza.find("div", {"class": "price"}).findAll("div")[0].text
+        pizzaSmallPrice = re.sub(r"\D", "", temp)
+        temp = pizza.find("div", {"class": "price"}).findAll("div")[1].text
+        pizzaMidPrice = re.sub(r"\D", "", temp)
+        temp = pizza.find("div", {"class": "price"}).findAll("div")[2].text
+        pizzaBigPrice = re.sub(r"\D", "", temp)
 
-	print("Nafn : " + pizzaName )
-	print("alegg : " + pizzaTopping)
-	print(listPizzaTopping)
-	print("litil verd: " + pizzaSmallPrice)
-	print("midstared : " + pizzaMidPrice)
-	print("stor verd : " + pizzaBigPrice)
+        # Don't add when pizza exists, TODO: Update pizza
+        if ScrapeMananger.pizza_exists(pizzaName, company_id):
+            continue
+
+        ScrapeMananger.add_scraped_pizza(name=pizzaName,
+                                         scraped_toppings=listPizzaTopping,
+                                         company_id=company_id,
+                                         s_price=pizzaSmallPrice,
+                                         m_price=pizzaMidPrice,
+                                         l_price=pizzaBigPrice)
