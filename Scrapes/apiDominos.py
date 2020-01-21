139efc88bad6b08edea190de004c3dff0df7ffda
eybbus <eythorfreyr94@gmail.com>
2020-01-21 01:08:39 +0000

    Dominos and pizza inserted and tested into database

---
+++ Scrapes/apiDominos.py
@@ -0,0 +1,43 @@
+import requests
+import Scrapes.scrapeMananger as ScrapeManager
+
+URL = 'https://api.dominos.is/api/menu'
+
+
+# create Test
+def scrape_dominos():
+    response = requests.get("https://api.dominos.is/api/menu")
+    company_id = ScrapeManager.insert_or_get_company(name='Dominos', region='höfuðborgarsvæðið').id
+    pizzas = response.json()
+    topping = ""
+    for pizza in pizzas['menu']['menuPizzas']:
+        pizzaName = pizza['name']
+
+        for i, top in enumerate(pizza['toppings']):
+            if i == 0:
+                topping += top['name']
+            else:
+                topping += ", " + top['name']
+        pizzaTopping = topping
+        listPizzaTopping = pizzaTopping.split(", ")
+        pizzaSmallPrice = pizza['sizes'][0]['pickupPrice']
+        pizzaMidPrice = pizza['sizes'][1]['pickupPrice']
+        pizzaBigPrice = pizza['sizes'][2]['pickupPrice']
+
+        topping = ""
+
+        # Don't add when pizza exists, TODO: Update pizza
+        if ScrapeManager.pizza_exists(pizzaName, company_id):
+            continue
+        ScrapeManager.add_scraped_pizza(name=pizzaName,
+                                        scraped_toppings=listPizzaTopping,
+                                        company_id=company_id,
+                                        s_price=pizzaSmallPrice,
+                                        m_price=pizzaMidPrice,
+                                        l_price=pizzaBigPrice)
+    # print("Nafn : " + pizzaName )
+    # print("alegg : " + pizzaTopping)
+    # print(listPizzaTopping)
+    # print(pizzaSmallPrice)
+    # print(pizzaMidPrice)
+    # print(pizzaBigPrice)
---
+++ Scrapes/apiPizzan.py
@@ -0,0 +1,38 @@
+import requests
+import Scrapes.scrapeMananger as ScrapeManager
+
+URL = 'https://api.pizzan.is/api/v1/pizzas/menu/'
+
+
+def scrape_pizzan():
+    response = requests.get(URL)
+
+    pizzas = response.json()
+
+    company_id = ScrapeManager.insert_or_get_company(name='Pizzan', region='höfuðborgarsvæðið').id
+
+    for pizza in pizzas:
+
+        pizzaName = pizza['name']
+        pizzaTopping = pizza['toppingsSummary'].lower()
+        listPizzaTopping = pizzaTopping.split(", ")
+        pizzaSmallPrice = pizza['minimumAmountSmall']
+        pizzaMidPrice = pizza['minimumAmountMedium']
+        pizzaBigPrice = pizza['minimumAmountLarge']
+
+        # Don't add when pizza exists, TODO: Update pizza
+        if ScrapeManager.pizza_exists(pizzaName, company_id):
+            continue
+        ScrapeManager.add_scraped_pizza(name=pizzaName,
+                                        scraped_toppings=listPizzaTopping,
+                                        company_id=company_id,
+                                        s_price=pizzaSmallPrice,
+                                        m_price=pizzaMidPrice,
+                                        l_price=pizzaBigPrice)
+
+        # print("Nafn : " + pizzaName)
+        # print("alegg : " + pizzaTopping)
+        # print(listPizzaTopping)
+        # print(pizzaSmallPrice)
+        # print(pizzaMidPrice)
+        # print(pizzaBigPrice)
