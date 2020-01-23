c8eec26816898ff4e1b90b72a516169f30ce8771
eybbus <eythorfreyr94@gmail.com>
2020-01-23 16:31:09 +0000

    Implemented headless selenium devitos scrape

--- Scrapes/webScrapeDevitos.py
+++ Scrapes/webScrapeDevitos.py
@@ -1,43 +1,64 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+# noinspection PyUnresolvedReferences
+import chromedriver_binary
 import time
-driver = webdriver.Firefox()
 
-driver.get('https://devitos.is/menu/')
-#webdriver.support.expected_conditions.presence_of_all_elements_located(driver.find_elements_by_xpath('//p[@class="menuItemName"]'))
-time.sleep(2)
-pizzaName = driver.find_elements_by_xpath('//p[@class="menuItemName"]')
-pizzaToppings = driver.find_elements_by_xpath('//p[@class="menuItemToppings"]')
-smallBasePrice = int(driver.find_element_by_id('basePrice10').text)
-medBasePrice = int(driver.find_element_by_id('basePrice12').text)
-bigBasePrice = int(driver.find_element_by_id('basePrice16').text)
-XLBasePrice = int(driver.find_element_by_id('basePrice18').text)
 
-smallToppingBasePrice = int(driver.find_element_by_id('price10').text)
-medToppingBasePrice = int(driver.find_element_by_id('price12').text)
-bigToppingBasePrice = int(driver.find_element_by_id('price16').text)
-XLToppingBasePrice = int(driver.find_element_by_id('price18').text)
-print(pizzaName[0].text)
-for i in range(4, len(pizzaName) - 1):
-	
-	pizzatoppingsList = pizzaToppings[i].text.replace(" og ",", " ).split(", ")
-	if(len(pizzatoppingsList)) > 1 :
-		print(pizzaName[i].text)
-		print(pizzatoppingsList)
-		'''for topping in pizzatoppingsList:
-			if topping == 'Ostur' or topping == 'Devitos sósa':
-				pizzatoppingsList.remove(topping)'''
-		pizzatoppingsList = [topping for topping in pizzatoppingsList if topping != 'Ostur' and topping != 'Devitos sósa']
+def get_html():
+    # this is slow, need to research faster methods, takes about 9 secondsprint('somthing')
+    chrome_options = Options()
+    print('start')
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument('--no-proxy-server')
+    chrome_options.add_argument("--proxy-server='direct://'")
+    chrome_options.add_argument("--proxy-bypass-list=*")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(options=chrome_options)
+    print('get')
+    driver.implicitly_wait(20)
+    driver.get('https://devitos.is/menu/')
+    time.sleep(1)
+    page = driver.page_source
+    print('quit')
+    driver.quit()
+    return page
 
-		pizzaSmallPrice = len(pizzatoppingsList) * smallToppingBasePrice + smallBasePrice
-		pizzaMedPrice = len(pizzatoppingsList) * medToppingBasePrice + medBasePrice
-		pizzaBigPrice = len(pizzatoppingsList) * bigToppingBasePrice + bigBasePrice
-		pizzaXLPrice = len(pizzatoppingsList) * XLToppingBasePrice + XLBasePrice
-		
-		print(pizzaSmallPrice)
-		print(pizzaMedPrice)
-		print(pizzaBigPrice)
-		print(pizzaXLPrice)
 
-driver.close()
+def scrape_devitos():
+    soup = BeautifulSoup(get_html(), "html.parser")
 
-	#print(pizzaMidPrice[i].text)
+    price_container = soup.find('div', {'class': 'toppings-price-style'})
+    menu_container = soup.find('div', {'id': 'main-menu'})
+
+    smallBasePrice = int(price_container.find('div', {'id': 'basePrice10'}).text)
+    medBasePrice = int(price_container.find('div', {'id': 'basePrice12'}).text)
+    bigBasePrice = int(price_container.find('div', {'id': 'basePrice16'}).text)
+    XLBasePrice = int(price_container.find('div', {'id': 'basePrice18'}).text)
+    smallToppingBasePrice = int(price_container.find('div', {'id': 'price10'}).text)
+    medToppingBasePrice = int(price_container.find('div', {'id': 'price12'}).text)
+    bigToppingBasePrice = int(price_container.find('div', {'id': 'price16'}).text)
+    XLToppingBasePrice = int(price_container.find('div', {'id': 'price18'}).text)
+
+    pizza_elm = menu_container.find_all('div', {'class': 'menu-item'})
+
+    for item in pizza_elm:
+        pizzaName = item.find('p', {'class': 'menuItemName'}).text
+        if 'tilboð' in pizzaName.lower():
+            continue
+        pizzatoppingsList = item.find('p', {'class': 'menuItemToppings'}).text.replace(" og ", ", ").lower().split(", ")
+        if(len(pizzatoppingsList)) > 1:
+            print(pizzaName)
+            pizzatoppingsList = [topping for topping in pizzatoppingsList if topping != 'ostur' and topping != 'devitos sósa']
+            print(pizzatoppingsList)
+            pizzaSmallPrice = len(pizzatoppingsList) * smallToppingBasePrice + smallBasePrice
+            pizzaMedPrice = len(pizzatoppingsList) * medToppingBasePrice + medBasePrice
+            pizzaBigPrice = len(pizzatoppingsList) * bigToppingBasePrice + bigBasePrice
+            pizzaXLPrice = len(pizzatoppingsList) * XLToppingBasePrice + XLBasePrice
+
+            print(pizzaSmallPrice)
+            print(pizzaMedPrice)
+            print(pizzaBigPrice)
+            print(pizzaXLPrice)
